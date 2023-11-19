8d435ca03f97db9b53abebea19245e49f2bad60f
cristiifrim <cristian.ifrim@tss-yonder.com>
2023-11-19 23:26:25 +0200

    added 2nd symbol table

--- Lab3/iProVLanguage/Program.cs
+++ Lab3/iProVLanguage/Program.cs
@@ -8,13 +8,15 @@ main.Run();
 
 public class BaseProgram
 {
-    private SymbolTable st;
+    private SymbolTable identifierSymbolTable;
+    private SymbolTable constantSymbolTable;
     private ProgramInternalForm pif;
     private Scanner scanner;
 
     public BaseProgram()
     {
-        st = new SymbolTable();
+        identifierSymbolTable = new SymbolTable();
+        constantSymbolTable = new SymbolTable();
         pif = new ProgramInternalForm();
         scanner = new Scanner();
     }
@@ -58,13 +60,13 @@ public class BaseProgram
                     }
                     else if (scanner.IsIdentifier(tokens[i]))
                     {
-                        st.AddSymbol(tokens[i]);
-                        pif.Add("id", st.GetSymbolValue(tokens[1]));
+                        identifierSymbolTable.AddSymbol(tokens[i]);
+                        pif.Add("id", identifierSymbolTable.GetSymbolValue(tokens[1]));
                     }
                     else if (scanner.IsConstant(tokens[i]))
                     {
-                        st.AddSymbol(extra + tokens[i]);
-                        int constant = st.GetSymbolValue(extra + tokens[i]);
+                        constantSymbolTable.AddSymbol(extra + tokens[i]);
+                        int constant = identifierSymbolTable.GetSymbolValue(extra + tokens[i]);
                         extra = "";
                         pif.Add("const", constant);
                     }
@@ -78,7 +80,12 @@ public class BaseProgram
 
         using (StreamWriter writer = new StreamWriter(OutputPaths.SymbolTable))
         {
-            writer.Write(st.ToString());
+            writer.Write(identifierSymbolTable.ToString());
+        }
+
+        using (StreamWriter writer = new StreamWriter(OutputPaths.SymbolTable, true))
+        {
+            writer.Write(constantSymbolTable.ToString());
         }
 
         using (StreamWriter writer = new StreamWriter(OutputPaths.ProgramInternalForm))
--- Lab3/iProVLanguage/Utils/Constants.cs
+++ Lab3/iProVLanguage/Utils/Constants.cs
@@ -13,20 +13,20 @@
 
     public static class ProgramPaths
     {
-        public const string Problem1 = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\iProVLanguage\\Resources\\p1.txt";
-        public const string Problem1Error = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\iProVLanguage\\Resources\\p1err.txt";
-        public const string Problem2 = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\iProVLanguage\\Resources\\p2.txt";
-        public const string Problem3 = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\iProVLanguage\\Resources\\p3.txt";
+        public const string Problem1 = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\Formal-Languages-and-Compiler-Design\\Lab3\\iProVLanguage\\Resources\\p1.txt";
+        public const string Problem1Error = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\Formal-Languages-and-Compiler-Design\\Lab3\\iProVLanguage\\Resources\\p1err.txt";
+        public const string Problem2 = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\Formal-Languages-and-Compiler-Design\\Lab3\\iProVLanguage\\Resources\\p2.txt";
+        public const string Problem3 = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\Formal-Languages-and-Compiler-Design\\Lab3\\iProVLanguage\\Resources\\p3.txt";
     }
 
     public static class OutputPaths
     {
-        public const string ProgramInternalForm = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\iProVLanguage\\Resources\\pif.out";
-        public const string SymbolTable = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\iProVLanguage\\Resources\\st.out";
+        public const string ProgramInternalForm = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\Formal-Languages-and-Compiler-Design\\Lab3\\iProVLanguage\\Resources\\pif.out";
+        public const string SymbolTable = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\Formal-Languages-and-Compiler-Design\\Lab3\\iProVLanguage\\Resources\\st.out";
     }
 
     public static class InputPaths
     {
-        public const string Tokens = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\iProVLanguage\\Resources\\Tokens.in";
+        public const string Tokens = "C:\\Users\\cifrim\\Desktop\\iProVLanguage\\Formal-Languages-and-Compiler-Design\\Lab3\\iProVLanguage\\Resources\\Tokens.in";
     }
 }
